27f23671d6b0fffe8b90851ea0ec4200912df5bd
Nicholas Kipkoech <nickey968@gmail.com>
2025-03-11 18:00:11 +0300

    design login page

--- app/login.tsx
+++ app/login.tsx
@@ -1,13 +1,36 @@
-import { SafeAreaView, StyleSheet, TextInput, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 const Login = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.textInputsBox}>
-        <TextInput style={styles.textInputs} />
-        <TextInput style={styles.textInputs} />
+    <View style={styles.container}>
+      <View style={styles.topContainer}></View>
+      <View style={styles.loginBox}>
+        <TextInput style={styles.textInputs} placeholder="Email" />
+        <TextInput
+          style={styles.textInputs}
+          placeholder="Password"
+          secureTextEntry
+        />
+
+        <TouchableOpacity style={styles.signInBtn}>
+          <Text
+            style={{
+              color: "white",
+              fontSize: 18,
+              fontWeight: "700",
+            }}
+          >
+            Sign in
+          </Text>
+        </TouchableOpacity>
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
 export default Login;
@@ -16,19 +39,43 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    backgroundColor: "whitesmoke",
+    backgroundColor: "#F8ECC2", // Ensure smooth transition
   },
-  textInputsBox: {
-    display: "flex",
-    flexDirection: "column",
+  topContainer: {
+    backgroundColor: "#F8ECC2",
+    height: "40%",
+  },
+  loginBox: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 20,
     gap: 20,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    width: "100%",
+    alignSelf: "center",
+    elevation: 5, // Adds shadow for depth (Android)
+    shadowColor: "#000", // iOS shadow
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    borderWidth: 0, // Remove border since background color is set
   },
   textInputs: {
     borderWidth: 0.5,
-    height: 50,
+    borderColor: "#ccc",
+    height: 55,
     padding: 8,
-    marginLeft: 10,
-    marginRight: 10,
     borderRadius: 8,
+    backgroundColor: "#F5F5F5",
+  },
+  signInBtn: {
+    height: 55,
+    backgroundColor: "#EE6856",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+    marginTop: 20,
   },
 });
